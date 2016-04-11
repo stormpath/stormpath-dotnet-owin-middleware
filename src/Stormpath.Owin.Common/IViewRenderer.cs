a627422e55bf8a445c928a706374c4cd2043ed30
Nate Barbettini <nathanaelb@gmail.com>
2016-04-11 15:37:17 -0700

    Doc fix

--- src/Stormpath.Owin.Common/IViewRenderer.cs
+++ src/Stormpath.Owin.Common/IViewRenderer.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="StormpathMiddleware.Initialize.cs" company="Stormpath, Inc.">
+﻿// <copyright file="IViewRenderer.cs" company="Stormpath, Inc.">
 // Copyright (c) 2016 Stormpath, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
