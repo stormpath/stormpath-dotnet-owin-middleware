1c278c3fba9b784bb3ec8b70738e8e838f97f043
Nate Barbettini <nathanaelb@gmail.com>
2016-09-14 18:07:32 -0700

    Add a null view renderer

---
+++ src/Stormpath.Owin.Abstractions/NullViewRenderer.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Stormpath.Owin.Abstractions
+{
+    public class NullViewRenderer : IViewRenderer
+    {
+        public Task<bool> RenderAsync(string name, object model, IOwinEnvironment context, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(false);
+        }
+    }
+}
