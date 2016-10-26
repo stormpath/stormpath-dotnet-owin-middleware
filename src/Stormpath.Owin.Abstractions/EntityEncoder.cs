ae71df39ddf9c9dfa671f190f52635955946226b
Nate Barbettini <nathanaelb@gmail.com>
2016-10-25 18:19:09 -0700

    Add entity encoder

---
+++ src/Stormpath.Owin.Abstractions/EntityEncoder.cs
@@ -0,0 +1,21 @@
+﻿namespace Stormpath.Owin.Abstractions
+{
+    public static class EntityEncoder
+    {
+        public static string Encode(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            return input
+                .Replace("&", "&amp;")
+                .Replace("<", "&lt;")
+                .Replace(">", "&gt;")
+                .Replace("\"", "&quot;")
+                .Replace("'", "&#x27;")
+                .Replace("/", "&#x2F;");
+        }
+    }
+}
---
+++ test/Stormpath.Owin.UnitTest/EntityEncoderShould.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Stormpath.Owin.Abstractions;
+using Xunit;
+
+namespace Stormpath.Owin.UnitTest
+{
+    public class EntityEncoderShould
+    {
+        [Fact]
+        public void EncodeAmpersand() => EntityEncoder.Encode("stuff&").Should().Be("stuff&amp;");
+
+        [Fact]
+        public void EncodeAngles() => EntityEncoder.Encode("<script>").Should().Be("&lt;script&gt;");
+
+        [Fact]
+        public void EncodeQuotes() => EntityEncoder.Encode("that's cool\"").Should().Be("that&#x27;s cool&quot;");
+
+        [Fact]
+        public void EncodeSlash() => EntityEncoder.Encode("/closing").Should().Be("&#x2F;closing");
+    }
+}
