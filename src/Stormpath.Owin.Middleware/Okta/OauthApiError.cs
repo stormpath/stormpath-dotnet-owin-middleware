0f5ef0a607b8ec09bf2644c0f9e9a910744391b4
Nate Barbettini <nathanaelb@gmail.com>
2017-05-17 20:25:52 -0700

    Improve login failure error message

---
+++ src/Stormpath.Owin.Middleware/Okta/OauthApiError.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Stormpath.Owin.Middleware.Okta
+{
+    public class OauthApiError
+    {
+        public string Error { get; set; }
+
+        [JsonProperty("error_description")]
+        public string Description { get; set; }
+    }
+}
