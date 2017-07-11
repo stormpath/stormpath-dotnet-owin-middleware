96b78a990111ccab2dd806d2aa38e6dba574231b
Nate Barbettini <nathanaelb@gmail.com>
2017-07-11 16:25:21 -0700

    Add groups expansion back (closes #91)

---
+++ src/Stormpath.Owin.Middleware/Model/MeGroupsCollectionModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Stormpath.Owin.Middleware.Model
+{
+    public sealed class MeGroupsCollectionModel
+    {
+        public int Size { get; set; }
+
+        public MeGroupModel[] Items { get; set; }
+    }
+}
